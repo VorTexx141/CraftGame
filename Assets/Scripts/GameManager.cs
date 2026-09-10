3cbc54828c9fee1795b5fbec73dc3792467c2706
ArtistInAVoid <thatdudewholives@gmail.com>
2026-09-10 15:55:56 +0300

    messed around with the game changing system. no longer should load a new scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public GameObject WinScreen;
+    public GameObject LoseScreen;
+    public GameObject[] minigames;
+    int activeMiniGame = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Shuffle()
+    {
+        for (int i = 0; i < minigames.Length; i++)
+        {
+            GameObject temp = minigames[i];
+            int randomIndex = Random.Range(i, minigames.Length);
+            minigames[i] = minigames[randomIndex];
+            minigames[randomIndex] = temp;
+        }
+    }
     private void Awake()
     {
+        Shuffle();
         if (instance != null)
         {
             Destroy(gameObject);
@@ -19,14 +34,15 @@ public class GameManager : MonoBehaviour
     
     public void wingame()
     {
-        StartCoroutine(LevelLoader());
+        StartCoroutine(GameChanger());
     }
-    IEnumerator LevelLoader()
+    IEnumerator GameChanger()
     {
-        AsyncOperation loading = SceneManager.LoadSceneAsync(1);
-        while(!loading.isDone)
+        WinScreen.SetActive(true);
+        yield return new WaitForSeconds(1);
+        if (activeMiniGame != null)
         {
-            yield return null;
+            minigames[activeMiniGame].SetActive(false);
         }
     }
 
